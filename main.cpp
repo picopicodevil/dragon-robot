aad1f5112ac515c77ad9229b35cb958091b5db09
picopicodevil <57719497+picopicodevil@users.noreply.github.com>
2020-10-21 15:21:10 +0900

    Update: インスタンスの名前の変更

--- main.cpp
+++ main.cpp
@@ -8,18 +8,19 @@
 DP_TB6643 wheel_right(p28, p22);
 DP_TB6643 wheel_left(p27, p21);
 
-TB6643 motor1(p26, p25);
-TB6643 motor2(p24, p23);
+TB6643 link_motor(p26, p25);
+TB6643 ball_screw_motor(p24, p23);
 
 LineTrace line_trace(p20, p19, p18);
 
 // RN4020 rn4020_in(p9, p10);
 // RN4020 rn4020_out(p13, p14);
 
-QEI encoder1(p5, p6, NC, 512, QEI::X4_ENCODING);
-QEI encoder2(p7, p8, NC, 512, QEI::X4_ENCODING);
+QEI link_encoder(p5, p6, NC, 512, QEI::X4_ENCODING);
+QEI ball_screw_encoder(p7, p8, NC, 512, QEI::X4_ENCODING);
 
-DigitalIn limit_sw(p12, PullDown);
+DigitalIn link_sw(p12, PullDown);
+DigitalIn ball_screw_sw(p14, PullDown);
 
 BufferedSerial pc(USBTX, USBRX, 115200);
 
@@ -40,14 +41,14 @@ int main()
 
     while (true)
     {
-        if (limit_sw == 1)
+        if (link_sw == 1)
         {
             1;
         }
 
-        encoder1.getRevolutions();
+        link_encoder.getRevolutions();
 
-        encoder2.getRevolutions();
+        ball_screw_encoder.getRevolutions();
 
         if (1)
         {
